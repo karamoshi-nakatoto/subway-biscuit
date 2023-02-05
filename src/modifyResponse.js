f30eee287c7ac9db6dbe961c0c66157b76518981
Karamoshi Nakatoto <karamoshi.nakatoto@protonmail.com>
2023-02-05 10:48:46 +0200

    feat: simplify regex

--- src/modifyResponse.js
+++ src/modifyResponse.js
@@ -3,14 +3,14 @@ function padWithZero(number) {
 }
 
 export function modifyResponse(response) {
-  const dateRegex = /Изтича на: 2\/9\/2023/g;
+  const dateRegex = /\$EXPIRATION_DATE/g;
   // What we know:
   // The first part is unknown (10)
   // The second part is the restaurant code (53790)
   // The third part is an unknown but probably a boolean (0)
   // The fourth part is the month and date (0204)
   // The fifth part is the time - hours and minutes (0301)
-  const codeRegex = /Код на оферта: 10-53790-0-0204-0301/g;
+  const codeRegex = /\$COUPON_CODE/g;
 
   // The coupons are usually valid for 5 days but we only add 4 to be on the
   // safe side - you never know what timezone bugs could exist.
@@ -30,7 +30,7 @@ export function modifyResponse(response) {
   const replacementCode = `10-53790-0-${fourthPart}-${fifthPart}`;
 
   const newResponse = response
-    .replace(dateRegex, `Изтича на: ${in4DaysString}`)
+    .replace(dateRegex, in4DaysString)
     .replace(codeRegex, replacementCode);
 
   return newResponse;
